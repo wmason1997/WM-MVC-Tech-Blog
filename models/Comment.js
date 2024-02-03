441f248771659671fa2a7597acc04dcaa0330712
wmason1997 <williamcmason1997@gmail.com>
2024-02-03 13:30:34 -0800

    Started filling out the models section.

--- models/Comment.js
+++ models/Comment.js
@@ -1,28 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Blog extends Model {}
+class Comment extends Model {}
 
-Blog.init(
+Comment.init(
     {
         id: {
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
             autoIncrement: true,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        content: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        date_created: {
-            type: DataTypes.DATE,
-            allowNull: false,
-            defaultValue: DataTypes.NOW,
         },
         user_id: {
             type: DataTypes.INTEGER,
@@ -31,14 +18,25 @@ Blog.init(
                 key: 'id',
             },
         },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'blog',
+                key: 'id'
+            },
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
     },
     {
         sequelize,
         timestamps: false,
-        freezeTableName: true,
         underscored: true,
-        modelName: 'blog',
+        modelName: 'comment',
     }
 );
 
-module.exports = Blog;
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+//Do all the associations
+
+module.exports = { User, Blog, Comment };
