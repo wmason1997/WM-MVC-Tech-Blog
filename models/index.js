a8a1ce3359d5568b4ff4e80e5e487697dcd5c40d
wmason1997 <williamcmason1997@gmail.com>
2024-02-03 13:54:27 -0800

    Added model associations in models/index.js

--- models/index.js
+++ models/index.js
@@ -4,4 +4,34 @@ const Comment = require('./Comment');
 
 //Do all the associations
 
+// User - Blog association
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+// User - Comment association
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Blog - Comment association
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Blog, Comment };
