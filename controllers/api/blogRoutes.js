69b57eaa26796f0143d472e06201074df94a3e84
wmason1997 <williamcmason1997@gmail.com>
2024-02-03 16:35:45 -0800

    Added code for blogRoutes.js

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const { Blog } = require('../../models');
+const withAuth = require ('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+    } catch (error) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if(!blogData) {
+            res.status(404).json({ message: 'No blog found with this id!' });
+            return;
+        }
+        
+        res.status(200).json(blogData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+module.exports = router;
