1ac7eb2284eb1c5576a25018dbdd614e2e8b0f24
wmason1997 <williamcmason1997@gmail.com>
2024-02-03 16:11:22 -0800

    Modified blogData.json for examples and seeds/seed.js files

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Blog, Comment } = require('../models');
 
 const userData = require('./userData.json');
 //const projectData = require('./projectData.json'); // switch this to fit this particular project
+
+const blogData = require('./blogData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -19,7 +21,17 @@ const seedDatabase = async () => {
 //       user_id: users[Math.floor(Math.random() * users.length)].id,
 //     });
 //   }
+// Maybe also with comments
 
+  for (const blog of blogData) {
+    await Blog.create({
+      ...blog,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+
+  
   process.exit(0);
 };
 
